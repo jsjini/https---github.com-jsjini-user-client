e18e9b02855d105034006bc227a8ff58c42f268f
JeonSangJin <jsjini92@naver.com>
2024-02-05 17:53:09 +0900

    2024-02-05 수업

---
+++ src/views/UserListView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <h1>전체 회원 조회</h1>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      userList : []
+    }
+  },
+  created() {
+    this.getUserList(); // 비동기작업
+  },
+  methods : {
+    async getUserList() { // 동기작업
+      let result = await axios.get('')
+                              .catch(err => console.log(err));
+      let list = result.data;
+      this.userList = list;
+    }
+  }
+}
+</script>
